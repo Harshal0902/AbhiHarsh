f073c5cf01be4b913d6991bcf6be0911f50f565a
Harshal Raikwar <64153988+Harshal0902@users.noreply.github.com>
2020-11-19 23:40:53 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="py-5" style={{background: "linear-gradient(315deg, #6617cb 0%, #882463 74%)"}}>
+        <footer className="py-5" style={{ background: "linear-gradient(315deg, #6617cb 0%, #882463 74%)" }}>
             <div className="container">
-                <p className="m-0 text-center text-white"> Made by <a href="#" target="_blank" style={{textDecoration: 'none'}}>&nbsp; Abhinav &nbsp;</a> and <a
-        href="https://harshal09.netlify.app/" target="_blank" style={{textDecoration: 'none'}}>&nbsp; Harshal</a>
-    </p>
+                <p className="m-0 text-center text-white"> Made by <a href="#" target="_blank" style={{ textDecoration: 'none' }}>&nbsp; Abhinav &nbsp;</a> and <a
+                    href="https://harshal09.netlify.app/" target="_blank" style={{ textDecoration: 'none' }}>&nbsp; Harshal</a>
+                </p>
             </div>
         </footer>
     );
