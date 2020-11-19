4c5c663485e5ab47809ed358454b0d1078386057
Harshal Raikwar <64153988+Harshal0902@users.noreply.github.com>
2020-11-19 22:22:51 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import './Footer.scss';
 
 const Footer = () => {
     return (
-        <footer className="footer">
-        Made by <a href="#" target="_blank"> &nbsp; Abhinav &nbsp; </a> and <a
-        href="https://harshal09.netlify.app/" target="_blank">&nbsp; Harshal</a>
-    </footer>
+        <footer className="py-5" style={{background: "linear-gradient(315deg, #6617cb 0%, #882463 74%)"}}>
+            <div className="container">
+                <p className="m-0 text-center text-white"> Made by <a href="#" target="_blank" style={{textDecoration: 'none'}}>&nbsp; Abhinav &nbsp;</a> and <a
+        href="https://harshal09.netlify.app/" target="_blank" style={{textDecoration: 'none'}}>&nbsp; Harshal</a>
+    </p>
+            </div>
+        </footer>
     );
 };
 
