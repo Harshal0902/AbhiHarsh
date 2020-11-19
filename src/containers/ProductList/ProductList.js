5d6ac943624a2350631549f80581646b9f24e035
Harshal Raikwar <64153988+Harshal0902@users.noreply.github.com>
2020-11-19 23:37:48 +0530

    Update ProductList.js

--- src/containers/ProductList/ProductList.js
+++ src/containers/ProductList/ProductList.js
@@ -1,17 +1,15 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Product from "../../components/Product/Product";
-
-import {brandFilter} from "../../pipes/brandFilter";
-import {orderByFilter} from "../../pipes/orderByFilter";
+import { brandFilter } from "../../pipes/brandFilter";
+import { orderByFilter } from "../../pipes/orderByFilter";
 import LayoutMode from "../../components/LayoutMode/LayoutMode";
-import {paginationPipe} from "../../pipes/paginationFilter";
+import { paginationPipe } from "../../pipes/paginationFilter";
 import Pagination from "../../components/Pagination/Pagination";
 
 class ProductList extends Component {
-
     state = {
-        colValue : 'col-lg-4',
+        colValue: 'col-lg-4',
         perPage: 12,
         currentPage: 1,
         pagesToShow: 3,
@@ -19,24 +17,24 @@ class ProductList extends Component {
     };
 
     changeLayout = (n) => {
-        this.setState({gridValue: n});
+        this.setState({ gridValue: n });
 
         let realGridValue;
 
-        if(n === 4) {
+        if (n === 4) {
             realGridValue = 3
         } else {
             realGridValue = 4;
         }
 
-      this.setState({
-          colValue: `col-lg-${realGridValue}`
-      });
+        this.setState({
+            colValue: `col-lg-${realGridValue}`
+        });
     };
 
 
     onPrev = () => {
-        const updatedState = {...this.state};
+        const updatedState = { ...this.state };
         updatedState.currentPage = this.state.currentPage - 1;
         this.setState(updatedState);
     };
@@ -59,7 +57,7 @@ class ProductList extends Component {
 
     render() {
 
-        let isActive = this.state.colValue[this.state.colValue.length -1];
+        let isActive = this.state.colValue[this.state.colValue.length - 1];
 
         return (
             <div className="col-lg-9">
@@ -69,13 +67,13 @@ class ProductList extends Component {
                             <div className="card-header d-flex justify-content-end">
                                 <span className="mr-3">Change Layout: </span>
                                 <LayoutMode len={3} isActive={this.state.gridValue === 3} click={this.changeLayout} />
-                                <LayoutMode len={4} isActive={this.state.gridValue === 4}  click={this.changeLayout} />
+                                <LayoutMode len={4} isActive={this.state.gridValue === 4} click={this.changeLayout} />
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="row">
-                    {paginationPipe(this.props.products, this.state).map(product =>{
+                    {paginationPipe(this.props.products, this.state).map(product => {
                         let classes = `${this.state.colValue} col-md-6 mb-4`;
                         return (<div className={classes}>
                             <Product key={product.id} product={product} />
@@ -106,7 +104,7 @@ const mapStateToProps = state => {
     const filterByOrderArr = orderByFilter(filterByBrandArr, orderBy);
 
 
-    return {products: filterByOrderArr }
+    return { products: filterByOrderArr }
 };
 
 export default connect(mapStateToProps, null)(ProductList);
