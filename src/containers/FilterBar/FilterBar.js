b9805e4e5c99fc56c2bb2d57ccf245c32fde5256
Harshal Raikwar <64153988+Harshal0902@users.noreply.github.com>
2020-11-19 22:27:24 +0530

    Update FilterBar.js

--- src/containers/FilterBar/FilterBar.js
+++ src/containers/FilterBar/FilterBar.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import BrandFilter from "../../components/BrandFilter/BrandFilter";
 import OrderFilter from "../../components/OrderFilter/OrderFilter";
 import './Filterbar.scss';
@@ -6,19 +6,20 @@ import './Filterbar.scss';
 class FilterBar extends Component {
     render() {
         return (
+
             <div className="col-lg-3">
-                <div className="row">
-                    <div id="gradient">
-            <form class="searchbox" action="#">
-                    <input type="search" placeholder="Search" />
-                    <button type="submit" value="search">&nbsp;</button>
-                </form>
+                <div id="gradient">
+                    <form class="searchbox" action="#">
+                        <input type="search" placeholder="Search" />
+                        <button type="submit" value="search">&nbsp;</button>
+                    </form>
                 </div>
-                <div className="col-12">
-                        <OrderFilter/>
+                <div className="row">
+                    <div className="col-12">
+                        <OrderFilter />
                     </div>
                     <div className="col-12">
-                        <BrandFilter/>
+                        <BrandFilter />
                     </div>
                 </div>
             </div>
