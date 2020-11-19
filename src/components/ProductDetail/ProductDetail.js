256d8d1d9b5e6535477085cc292708822d5b7d85
Harshal Raikwar <64153988+Harshal0902@users.noreply.github.com>
2020-11-19 23:46:43 +0530

    Update ProductDetail.js

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {formatMoney} from "../../pipes/priceFormatter";
-import {addProductToCart} from "../../actions";
+import { connect } from 'react-redux';
+import { formatMoney } from "../../pipes/priceFormatter";
+import { addProductToCart } from "../../actions";
 
 const ProductDetail = (props) => {
 
     const {
         title,
-                images,
-                brand,
-                price,
-                wire,
-                batterycell,
-                size,
-                weight,
-                display,
-                battery,
-                rating,
-                description,
-                id
+        images,
+        brand,
+        price,
+        wire,
+        batterycell,
+        size,
+        weight,
+        display,
+        battery,
+        rating,
+        description,
+        id
     } = props.product;
 
 
@@ -27,55 +27,55 @@ const ProductDetail = (props) => {
     };
 
     return (
-      
+
         <aside className="col-sm-7">
             <article className="card-body p-5">
                 <h3 className="title mb-3">{title}</h3>
                 <div className="box">
-                <p className="price-detail-wrap">
-	<span className="price h3 text-warning">
-    
-		<span className="currency">Rs. </span><span className="num">{formatMoney(price)}</span>
-	</span>
-                </p>
-                <dl className="item-property">
-                     <dt>Description</dt>
-                     <dd><p className="text-capitalize">{description}</p></dd>
-                 </dl>
-                 <dl className="param param-feature">
-                     <dt>Brand</dt>
-                     <dd className="text-capitalize">{brand}</dd>
-                 </dl>
-                 <dl className="param param-feature">
-                     <dt>Size</dt>
-                     <dd>{size}</dd>
-                 </dl>
-                 <dl className="param param-feature">
-                     <dt>Battery Cell Composite</dt>
-                     <dd>{batterycell}</dd>
-                 </dl>
-                 <dl className="param param-feature">
-                     <dt>Wireless Type</dt>
-                     <dd>{wire}</dd>
-                 </dl>
-                 <dl className="param param-feature">
-                     <dt>Rating</dt>                     
-                     <dd>{rating}</dd>
-              </dl>
-              <dl className="param param-feature">
-                   <dt>Display</dt>
-                   <dd>{display}</dd>
-                </dl>
-                <dl className="param param-feature">
-                    <dt>Battery</dt>
-                     <dd>{battery}</dd>
-                 </dl>
-                <hr/>
-                <hr/>
-                <button
-                    onClick={onCart}
-                    className="btn btn-lg btn-outline-primary text-uppercase"><i
-                    className="fa fa-shopping-cart"/> Add to cart
+                    <p className="price-detail-wrap">
+                        <span className="price h3 text-warning">
+
+                            <span className="currency">Rs. </span><span className="num">{formatMoney(price)}</span>
+                        </span>
+                    </p>
+                    <dl className="item-property">
+                        <dt>Description</dt>
+                        <dd><p className="text-capitalize">{description}</p></dd>
+                    </dl>
+                    <dl className="param param-feature">
+                        <dt>Brand</dt>
+                        <dd className="text-capitalize">{brand}</dd>
+                    </dl>
+                    <dl className="param param-feature">
+                        <dt>Size</dt>
+                        <dd>{size}</dd>
+                    </dl>
+                    <dl className="param param-feature">
+                        <dt>Battery Cell Composite</dt>
+                        <dd>{batterycell}</dd>
+                    </dl>
+                    <dl className="param param-feature">
+                        <dt>Wireless Type</dt>
+                        <dd>{wire}</dd>
+                    </dl>
+                    <dl className="param param-feature">
+                        <dt>Rating</dt>
+                        <dd>{rating}</dd>
+                    </dl>
+                    <dl className="param param-feature">
+                        <dt>Display</dt>
+                        <dd>{display}</dd>
+                    </dl>
+                    <dl className="param param-feature">
+                        <dt>Battery</dt>
+                        <dd>{battery}</dd>
+                    </dl>
+                    <hr />
+                    <hr />
+                    <button
+                        onClick={onCart}
+                        className="btn btn-lg btn-outline-primary text-uppercase"><i
+                            className="fa fa-shopping-cart" /> Add to cart
                 </button>
                 </div>
             </article>
