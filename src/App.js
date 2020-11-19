4f37ec82299e318789e614627ac71fc5801450e2
Harshal Raikwar <64153988+Harshal0902@users.noreply.github.com>
2020-11-19 23:25:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import {Provider} from 'react-redux';
 import {createStore } from 'redux';
 import rootReducer from './reducers';
+import ScrollTop from "./components/ScrollToTop/Scroll";
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
 import './App.scss';
 import Home from "./pages/Home/Home";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import ProductDetail from "./pages/ProductDetail/ProductDetail";
 import ShoppingCart from "./pages/ShopingCart/ShoppingCart";
-import ScrollTop from "./components/ScrollToTop/Scroll";
-import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
 
 export const  store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
@@ -29,9 +29,9 @@ class App extends Component {
                     <Route exact patr={'/cart'} component={ShoppingCart}/>
                 </Switch>
                 <Footer/>
-                <ScrollTop showBelow={200} />
             </React.Fragment>
             </BrowserRouter>
+            <ScrollTop showBelow={200} />
         </Provider>
     );
   }
